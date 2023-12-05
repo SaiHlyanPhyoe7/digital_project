823238d2ce07ba629d2ce68cc59ce6707d3787f0
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-12-05 11:02:00 +0630

    feat:Implement the navbar section

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,82 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Icons } from "./Icons";
+import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
+
+const Navbar = () => {
+  const user = null;
+
+  return (
+    <div>
+      <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+        <header className="relative bg-white">
+          <MaxWidthWrapper>
+            <div className="border-b border-gray-200">
+              <div className="flex h-16 items-center">
+                {/* TODO: Mobile nav */}
+
+                <div className="ml-4 flex lg:ml-0">
+                  <Link href="/">
+                    <Icons.logo className="h-10 w-10" />
+                  </Link>
+                </div>
+
+                <div className="hidden lg:block z-50 lg:ml-8 lg:self-stretch">
+                  <NavItems />
+                </div>
+
+                <div className="ml-auto flex items-center">
+                  <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                    {user ? null : (
+                      <Link
+                        className={buttonVariants({ variant: "ghost" })}
+                        href="sign-in"
+                      >
+                        Sign in
+                      </Link>
+                    )}
+
+                    {user ? null : (
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
+                      />
+                    )}
+
+                    {user ? (
+                      <p></p>
+                    ) : (
+                      <Link
+                        href="/sign-up"
+                        className={buttonVariants({ variant: "ghost" })}
+                      >
+                        Create Account
+                      </Link>
+                    )}
+
+                    {user ? (
+                      <span className="h-6 w-px bg-gray-200"></span>
+                    ) : null}
+
+                    {user ? null : (
+                      <div className="flex lg:ml-6">
+                        <span className="h-6 w-px bg-gray-200"></span>
+                      </div>
+                    )}
+
+                    <div className="ml-4 flow-root lg:ml-6">
+                      {/* <Cart /> */}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </MaxWidthWrapper>
+        </header>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
